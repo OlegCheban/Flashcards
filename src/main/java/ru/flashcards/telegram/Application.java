52c71919ba23f0f1d70e8a0850a7c7bb34e745d5
OlegCheban <oleg.cheban1989@gmail.com>
2024-10-30 23:16:25 +0300

    configuration deploy to a new server

--- src/main/java/ru/flashcards/telegram/Application.java
+++ src/main/java/ru/flashcards/telegram/Application.java
@@ -3,13 +3,14 @@ package ru.flashcards.telegram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.flashcards.telegram.bot.config.TelegramProperties;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties(TelegramProperties.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
